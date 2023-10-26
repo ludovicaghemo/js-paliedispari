3bfea8633d6f3424dcb87a69723879af55836c3e
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-10-26 17:29:07 +0200

    add: complete palindrom project

---
+++ palindroma/js/script.js
@@ -0,0 +1,31 @@
+// RACCOLTA DATI
+// Chiedere parola all'utente
+const userChoice = prompt("Scrivi qui la parola che hai scelto!");
+console.log(userChoice);
+
+// LOGICA DEL PROGRAMMA
+
+// Controllo della parola
+// Creare una funzione che analizzi la parola lettera per lettera al contrario
+
+function reverse(userWord) {
+    reverseWord = ""; 
+    for (let i = userWord.length -1; i >= 0; i--) {
+        const wordLetter = userWord[i];
+        reverseWord += wordLetter;
+    }
+    return reverseWordArray; 
+}
+
+// INVOCAZIONE DELLA FUNZIONE
+reverse(userChoice);
+console.log(reverse(userChoice));
+
+// ISTRUZIONE CONDIZIONALE
+if (userChoice === reverseWordArray ) {
+    console.log("Parola palindroma");
+} else {
+    console.log("Parola non palindroma");
+}
+
+
